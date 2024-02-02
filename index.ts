26ee5ac23e7df63c0a732f006b7dad5b558dc54b
Shrutimitranand Chaturvedi <shrutimitr@saras-it.co>
2024-02-02 13:26:27 +0100

    package updates

--- index.ts
+++ index.ts
@@ -26,7 +26,7 @@ async function run(): Promise<void> {
         const octokit = github.getOctokit(myToken);
 
         // Load release list from GitHub
-        let releaseList = await octokit.repos.listReleases({
+        let releaseList = await octokit.rest.repos.listReleases({
             repo: repo_name,
             owner: repo_owner,
             per_page: Number(topList),
@@ -83,11 +83,11 @@ async function run(): Promise<void> {
  * Setup action output values
  * @param release - founded release
  */
-function setOutput(release: { id: number, tag_name: string, created_at: string, draft: boolean, prerelease: boolean,upload_url: string }): void {
+function setOutput(release: Record<string, unknown>): void {
     core.setOutput('id', release.id);
     core.setOutput('name', release.id);
     core.setOutput('tag_name', release.tag_name);
-    core.setOutput('version', release.tag_name.replace('v', ''));
+    core.setOutput('version', String(release.tag_name).replace('v', ''));
     core.setOutput('created_at', release.created_at);
     core.setOutput('draft', release.draft);
     core.setOutput('prerelease', release.prerelease);
